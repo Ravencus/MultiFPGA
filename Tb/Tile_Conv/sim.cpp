5a2ee4cd6437eedb6cde81495d33525e35719e74
Zheng <zzheng345@ece-linlabsrv01.ece.gatech.edu>
2023-05-01 19:10:43 -0400

    TB simulation passed

--- Tb/Tile_Conv/sim.cpp
+++ Tb/Tile_Conv/sim.cpp
@@ -14,7 +14,8 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-
+#include <string>
+#include <bitset>
 #include "conv.h"
 
 // #define PRINT_DEBUG
@@ -27,7 +28,8 @@ float conv_layer_input_feature_map[3][736][1280];
 float conv_layer_weights[64][3][7][7];
 float conv_layer_bias[64];
 float conv_layer_golden_output_feature_map[64][368][640];
-ap_fixed<16,3> data[64 * 368 * 640];
+ap_fixed<16, 3> data[64 * 368 * 640];
+float golden_data[64 * 368 * 640];
 
 fm_t fixp_conv_layer_input_feature_map[3][736][1280];
 wt_t fixp_conv_layer_weights[64][3][7][7];
@@ -97,22 +99,42 @@ int main()
 
     ap_axis<16, 1, 1, 1> tmp;
     // flatten output feature map to 1d
-    
+
+    int llimit = 100;
+
     int i = 0;
+    int j = 10;
     while (true)
     {
-        if (board_output_feature_map.empty())
+        if (board_output_feature_map.empty() && j > 0)
         {
             std::cout << "empty" << std::endl;
+            j--;
+            if (j == 0)
+            {
+                break;
+            }
             continue;
         }
         else
         {
-            
 
             tmp = board_output_feature_map.read();
+            ap_int<16> raw = tmp.data;
+            ap_fixed<16, 3> fp;
+            for (int t = 0; t < 16; t++)
+            {
+                fp[t] = raw[t];
+            }
+
             // std::cout << tmp.data << std::endl;
-            data[i] = tmp.data;
+            data[i] = fp;
+            // std::cout << data[i] << std::endl;
+            if (llimit > 0)
+            {
+                // std::cout << "reading No. "<<100 - llimit<<": " << data[i] << std::endl;
+                llimit--;
+            }
             i++;
             if (i == 64 * 368 * 640)
             {
@@ -120,22 +142,53 @@ int main()
             }
         }
     }
-
+    int golden_data_index = 0;
     // unflatten output feature map to 3d
-
-    for (int f = 0; f < 64; f++)
+    int count = 200;
+    float local_tile[4][23][20];
+    for (int ti = 0; ti < 16; ++ti)
     {
-        for (int i = 0; i < 368; i++)
+        for (int tj = 0; tj < 32; ++tj)
         {
-            for (int j = 0; j < 640; j++)
+            if(tj == 32) {
+                // std::cout << "tile: " << ti << " " << tj << " " << tk << std::endl;
+                return 0;
+            }
+            for (int tk = 0; tk < 16; ++tk)
             {
-                fixp_conv_layer_output_feature_map[f][i][j] = data[f * 368 * 640 + i * 640 + j];
-                // if (fixp_conv_layer_output_feature_map[f][i][j] != 0)
-                // {
-                //     std::cout << "read: " << fixp_conv_layer_output_feature_map[f][i][j] << std::endl;
-                //     std::cout << "golden: " << conv_layer_golden_output_feature_map[f][i][j] << std::endl;
-                // }
+                // std::cout << "tile: " << ti << " " << tj << " " << tk << std::endl;
+                int i_start = ti * 23;
+                int j_start = tj * 20;
+                int k_start = tk * 4;
                 
+                // read in local tile
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 0; i < 23; i++)
+                    {
+                        for (int j = 0; j < 20; j++)
+                        {
+                            // std::cout << "i: " << i_start + i << " j: " << j_start + j << " k: " << k_start + k << std::endl;
+                            // std::cout << "bounds: "<< std::endl;
+                            // std::cout << "i: " << 64 << " j: " << 368 << " k: " << 640 << std::endl;
+                            local_tile[k][i][j] = conv_layer_golden_output_feature_map[k_start + k][i_start + i][j_start + j];
+                            
+                        }
+                    }
+                }
+
+                for (int k = 0; k < 4; k++)
+                {
+                    for (int i = 0; i < 23; i++)
+                    {
+                        for (int j = 0; j < 20; j++)
+                        {
+                            golden_data[golden_data_index] = local_tile[k][i][j];
+                            golden_data_index++;
+                            // std::cout << "i: " << i_start+i << " j: " << j_start+j << " k: " << k_start+k << " data: " << data[i_start+i][j_start+j][k_start+k] << std::endl;
+                        }
+                    }
+                }
             }
         }
     }
@@ -143,27 +196,32 @@ int main()
     std::cout << "Tiled-convolution simulation complete!\n"
               << std::endl;
 
-    // Compute Mean-Squared-Error
-    for (int f = 0; f < 64; f++)
-    {
-        for (int i = 0; i < 368; i++)
-        {
-            for (int j = 0; j < 640; j++)
-            {
-                mse += std::pow((conv_layer_golden_output_feature_map[f][i][j] - (float)fixp_conv_layer_output_feature_map[f][i][j]), 2);
-            }
-        }
-#ifdef PRINT_DEBUG
-        // Prints sample output values (first feature of each channel) for comparison
-        // Modify as required for debugging
-        int row = 367;
-        int col = 1279;
+    //     // Compute Mean-Squared-Error
+    //     for (int f = 0; f < 64; f++)
+    //     {
+    //         for (int i = 0; i < 368; i++)
+    //         {
+    //             for (int j = 0; j < 640; j++)
+    //             {
+    //                 mse += std::pow((conv_layer_golden_output_feature_map[f][i][j] - (float)fixp_conv_layer_output_feature_map[f][i][j]), 2);
+    //             }
+    //         }
+    // #ifdef PRINT_DEBUG
+    //         // Prints sample output values (first feature of each channel) for comparison
+    //         // Modify as required for debugging
+    //         int row = 367;
+    //         int col = 1279;
 
-        cout << "Output feature[" << f << "][" << row << "][" << col << "]: ";
-        cout << "Expected: " << conv_layer_golden_output_feature_map[f][row][col] << "\t";
-        cout << "Actual: " << fixp_conv_layer_output_feature_map[f][row][col];
-        cout << endl;
-#endif
+    //         cout << "Output feature[" << f << "][" << row << "][" << col << "]: ";
+    //         cout << "Expected: " << conv_layer_golden_output_feature_map[f][row][col] << "\t";
+    //         cout << "Actual: " << fixp_conv_layer_output_feature_map[f][row][col];
+    //         cout << endl;
+    // #endif
+    //     }
+    // mse for golden data and data
+    for (int i = 0; i < 64 * 368 * 640; i++)
+    {
+        mse += std::pow((golden_data[i] - (float)data[i]), 2);
     }
 
     mse = mse / (64 * 368 * 640);
