5b83d9decd88f8dd3c49e1078958971a57607327
Fan <zfan87@icsrl-srv2.ece.gatech.edu>
2023-05-01 19:56:01 -0400

    RBN_trial01

--- HLS/RBN/RBN_inf.cpp
+++ HLS/RBN/RBN_inf.cpp
@@ -5,8 +5,9 @@
 #define W 23     // IFM_ width
 #define H 20    // IFM_ height
 #include<math.h>
-#include <ap_fixed.h>
-
+#include "ap_axi_sdata.h"
+#include "hls_stream.h"
+#include "ap_int.h"
 typedef ap_fixed<16, 3> fm;
 
 
@@ -14,19 +15,16 @@ typedef ap_fixed<16, 3> fm;
 void RBN_inf(
 
 	fm op[C][H][W],
-	fm x[C][H][W]
+	// fm x[C][H][W]
+	hls::stream<ap_fixed<16, 3>> &stream_in
 )
 {
 
-#pragma HLS interface mode=m_axi port = x  bundle=x_ip
+#pragma HLS interface mode=axis port = stream_in  bundle=input
 #pragma HLS interface mode=m_axi port = op  bundle=y_op
 #pragma HLS INTERFACE s_axilite register port=return
 
-// #pragma HLS interface mode=m_axi offset=slave bundle=C port=mean
-// #pragma HLS interface mode=m_axi offset=slave bundle=D port=var
-// #pragma HLS interface mode=m_axi offset=slave bundle=C port=CN
-// #pragma HLS interface mode=m_axi offset=slave bundle=D port=beta
-// #pragma HLS interface mode=m_axi offset=slave bundle=D port=gamma
+
 	fm CN = 1;
 	fm mean[C];
 	fm var[C];
@@ -41,59 +39,74 @@ void RBN_inf(
 		gamma[i] = i;
 	}
 
-	fm x_B[C][H][W];
-	fm op_B[C][H][W];
-	// int mean_B[C];
-	// int var_B[C];
-	// int CN_B;
-	// int beta_B[C];
-	// int gamma_B[C];
+	// fm x_B[C][H][W];
+	// fm op_B[C][H][W];
 
-	for (int c=0; c<C; c++)
+	// bool tile_full = false;
+	ap_fixed<16, 3> dataholder;
+	
+	ap_fixed<16,3> local_tile[C][H][W];
+
+	int stream_ct = 0;
+	int idx_C, idx_W, idx_H;
+	int tile_count = 0;
+	while (true)
 	{
-		for (int h=0; h<H; h++)
+		if (!stream_in.empty())
 		{
-			for(int w=0; w<W; w++)
-			{
+			dataholder = stream_in.read()
+			 
+			idx_W = stream_ct % W;
+			idx_H = stream_ct / W;
+			idx_C = stream_ct / (H*W);
 
-				x_B[c][h][w] = x[c][h][w];
+			local_tile[idx_C][idx_H][idx_W] = dataholder;
+
+
+			stream_ct++;
+			if (stream_ct == C*H*W)
+			{
+				stream_ct = 0;
+				// compute RBN
+				for (int c = 0; c < C; c+=1)
+				{
+					for (int h = 0; h < H; h++)
+					{
+						for (int w = 0; w < W; w++)
+						{
+								op[c][h][w] = (local_tile[c][h][w] - mean[c]) / var[c] / CN * gamma[c] + beta[c];
+						}
+					}
+				}
+				tile_count+=1;
+				if (tile_count == 16*32*16){
+					return;
+				}
 			}
 		}
-	}
-
-//#pragma HLS pipeline off
-////#pragma HLS unroll
-//	for (int repli=0; repli<5; repli++)
-//	{
-
-	for (int c = 0; c < C; c+=1)
-	{
-		for (int h = 0; h < H; h++)
+		else
 		{
-// #pragma HLS pipeline
-			for (int w = 0; w < W; w++)
-			{
-				// for (int ww=0; ww < 4; ww++)
-					op_B[c][h][w] = (x_B[c][h][w] - mean[c]) / var[c] / CN * gamma[c] + beta[c];
-				// op_B[c][h][w+ww] = (x_B[c][h][w+ww] - mean[c]) / var[c] / CN * gamma[c] + beta[c];
-
-			}
+			continue;
 		}
+			
 	}
-//	}
 
-	for (int c = 0; c < C; c++)
-	{
-		for (int h = 0; h < H; h++)
-		{
-			for (int w = 0; w < W; w++)
-			{
-				op[c][h][w] = op_B[c][h][w];
-			}
 
-		}
 
-	}
+	
+
+	// for (int c = 0; c < C; c++)
+	// {
+	// 	for (int h = 0; h < H; h++)
+	// 	{
+	// 		for (int w = 0; w < W; w++)
+	// 		{
+	// 			op[c][h][w] = op_B[c][h][w];
+	// 		}
+
+	// 	}
+
+	// }
 
     return;
 }
